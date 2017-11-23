f53b15e05e4c120904dd6269f3eaf0838e4f9b7d
严彬荣 <453420698@qq.com>
2017-11-23 21:20:17 +0800

    修复供需信息查询不出数据报空指针错误

--- src/main/webapp/js/host.js
+++ src/main/webapp/js/host.js
@@ -1,3 +1,3 @@
-window.sessionStorage.setItem('host', 'http://172.16.13.113');
+window.sessionStorage.setItem('host', 'http://127.0.0.1:8080');
 //window.sessionStorage.setItem('host','http://222.85.156.50:81/');
 //window.sessionStorage.setItem('host','http://172.16.13.136:8080');
